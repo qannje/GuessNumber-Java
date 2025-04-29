5f55590a7cbb59db3626220781889f73669fcc82
Sebastian <seba05120@gmail.com>
2025-04-29 15:40:01 +0300

    add main file

---
+++ scr/main/java/Main.java
@@ -0,0 +1,10 @@
+import game.GameRunner;
+import game.NumberGuessingGame;
+
+
+public class Main {
+    public static void main(String[] args) {
+        GameRunner runner = new GameRunner();
+        runner.run();
+    }
+}
